de94433f9cca6a3e80cdc791aeb09177281634bf
Venkata Ramana <pvrreddy155@gmail.com>
2018-02-20 13:02:17 +0530

    Update image_grid_effect.js

--- files/image_grid_effect.js
+++ files/image_grid_effect.js
@@ -1,14 +1,6 @@
 window.onload = function() {
     
     document.getElementById("input").defaultValue = "10";
-    function effect(){
-        $("#img").html("");
-        val = $('input').val()
-        if(val == 0){
-            imageToGrid(1);
-        }
-        imageToGrid(val);
-    }
 
     imageToGrid(10);  //run function with required size
 
@@ -47,4 +39,14 @@ window.onload = function() {
             }
         }
     }
+    
+    $("button").click(function(){
+        $("#main").html("");
+        val = $('input').val()
+        if(val == 0){
+            imageToGrid(1);
+        }
+        imageToGrid(val);
+    })
+    
 }
