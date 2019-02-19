2a21356343e29691bcff7639650bb10f1e7b85bc
Venkata Ramana <pvrreddy155@gmail.com>
2019-02-19 16:16:35 +0530

    Update image_grid_effect.js

--- files/image_grid_effect.js
+++ files/image_grid_effect.js
@@ -1,10 +1,6 @@
-window.onload = function() {
-    
-    document.getElementById("input").defaultValue = "10";
 
-    imageToGrid(10);  //run function with required size with default width and height
-    
-    //imageToGrid(10, 300, 300) //run function with required size, custom width and custom height
+    // Image To Grid Effect
+    // Author: Venkata Ramana. P (github/itsmepvr)
 
     function imageToGrid(size, canvasWidth, canvasHeight){
     
@@ -56,13 +52,3 @@ window.onload = function() {
         }
     }
     
-    $("button").click(function(){
-        $("#main").html("");
-        val = $('input').val()
-        if(val == 0){
-            imageToGrid(1);
-        }
-        imageToGrid(val);
-    })
-    
-}
